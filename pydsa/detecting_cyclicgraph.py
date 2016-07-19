85630641f492e03e69a36c2ade2b253e599acd69
yatingupta10 <yatingupta12@gmail.com>
2016-07-19 22:39:24 +0530

    Code and test for detecting cyclic graph added

---
+++ pydsa/detecting_cyclicgraph.py
@@ -0,0 +1,20 @@
+def detecting_cyclicgraph(g):
+    """Return True if the directed graph g has a cycle.
+    g must be represented as a dictionary mapping vertices to
+    iterables of neighbouring vertices. 
+    """
+    path = set()
+    visited = set()
+
+    def visit(vertex):
+        if vertex in visited:
+            return False
+        visited.add(vertex)
+        path.add(vertex)
+        for neighbour in g.get(vertex, ()):
+            if neighbour in path or visit(neighbour):
+                return True
+        path.remove(vertex)
+        return False
+
+    return any(visit(v) for v in g)
